109647c9742f0fe7b0c9ac429f1c8839d46cdc7e
youtube <youtube@DESKTOP-FJ8UU5T>
2020-11-21 00:36:46 +0900

    # E10 1분코딩 : 배열(다중 값) 검색 기능구현 Mybatis foreach 사용

--- src/main/java/kr/co/songjava/mvc/parameter/BoardSearchParameter.java
+++ src/main/java/kr/co/songjava/mvc/parameter/BoardSearchParameter.java
@@ -1,5 +1,8 @@
 package kr.co.songjava.mvc.parameter;
 
+import java.util.List;
+
+import kr.co.songjava.mvc.domain.BoardType;
 import lombok.Data;
 
 /**
@@ -10,6 +13,7 @@ import lombok.Data;
 public class BoardSearchParameter {
 
 	private String keyword;
+	private List<BoardType> boardTypes;
 	
 	public BoardSearchParameter() {
 		
